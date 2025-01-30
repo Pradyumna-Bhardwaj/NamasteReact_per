85dcfd8069ae9134442b472865c169f427a54d34
Pradyumna <bhardwaj.pradyumn.pb@gmail.com>
2025-01-30 18:18:07 +0530

    shimmer UI started

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,29 @@
 import RestaurantCard from "./restaurantCard";
 import restaurantList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
+  {
+    let resList = []
+    if(resList.length===0){
+      return <h1>loading...</h1>;
+    }
+  }
   const [resList, setResList] = useState(restaurantList);
+  
+  // const fetchData = async () => {
+  //   const data = await fetch(
+  //     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+  //   );
+  //   const json = await data.json()
+  //   console.log(json)
+  // };
+
+  // useEffect(()=>{
+  //   fetchData();
+  // }, []);
+
+
   return (
     <div className="restaurant-list">
       <div className="filter">
@@ -11,7 +31,7 @@ const Body = () => {
           className="filter-btn"
           onClick={() => {
             filteredList = restaurantList.filter(
-              (res) => res.data.avgRating > 4
+              (res) => res.data.avgRating >= 4
             );
             setResList(filteredList); //resList is updated here to filtered list and virtual DOM is changed
           }}
